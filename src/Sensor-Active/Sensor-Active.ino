e9aa1f030e1ef8263ef0ab20609d4bc6bce631d1
Walwode <skorpion_msr@web.de>
2018-08-08 11:34:09 +0200

    Added passive and active sensor

---
+++ src/Sensor-Active/Sensor-Active.ino
@@ -0,0 +1,77 @@
+#include <SBNetwork.h>
+#include "PowerHelper.h"
+#include "Sensor.h"
+
+SBNetwork networkDevice(true, 9, 10);
+
+/*
+ * The active NRF24 device is sending sensor values in a specified time.
+ * Due to Radio.PowerDown, PowerSleep and ClockPreScaler, it's power consumption is 5.4uA in sleep mode.
+ *
+ * Uncomment and configure the sensor values in the loop section
+ */
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);
+
+  randomSeed(getRandomSeed());
+  SBMacAddress deviceMac(random(0,256), random(0,256), random(0,256), random(0,256), random(0,256));
+  networkDevice.initialize(deviceMac);
+
+  Serial.println(F("*** PRESS 'N' to reset the device"));
+}
+
+void loop() {
+  PowerHelper::setClockPrescaler(CLOCK_PRESCALER_1);
+  PowerHelper::resetADC();
+
+  if (Serial.available()) { char c = toupper(Serial.read()); if (c == 'N') { networkDevice.resetData(); } }
+  networkDevice.update();
+  
+  uint16_t voltage = Sensor::readVoltage(7, A6);
+  sendNrf24(SENSOR_TYPE_VOLTAGE, (float)voltage);
+  PowerHelper::sleep(8); // break to finish nrf24
+  
+  uint16_t humidity = Sensor::readHumidity(8, A0);
+  sendNrf24(SENSOR_TYPE_HUMIDITY, (float)humidity);
+  PowerHelper::sleep(8); // break to finish nrf24
+
+  uint16_t photo = Sensor::readPhoto(8, A1);
+  sendNrf24(SENSOR_TYPE_PHOTO, (float)photo);
+  PowerHelper::sleep(8); // break to finish nrf24
+  
+  byte temp, hum = 0;
+  Sensor::readDht11(7, 6, temp, hum);
+  sendNrf24(SENSOR_TYPE_TEMPERATURE, (float)temp);
+  sendNrf24(SENSOR_TYPE_HUMIDITYAIR, (float)hum);
+  PowerHelper::sleep(8); // break to finish nrf24
+  
+  PowerHelper::setClockPrescaler(CLOCK_PRESCALER_16);
+  PowerHelper::disableADC();
+  PowerHelper::sleep(1175);
+}
+
+void sendNrf24(uint8_t type, float value) {
+  Serial.println(F("[NRF24] Send data... "));
+  Serial.print(F("[NRF24] Type: "));
+  Serial.println(type);
+  Serial.print(F("[NRF24] Value: "));
+  Serial.println(value);
+  networkDevice.radio.powerUp();
+  
+  uint8_t version = 0x01;
+  byte message[1+1+4];
+  memcpy((void*)(message), &version, sizeof(version));
+  memcpy((void*)(message + 1), &type, sizeof(type));
+  memcpy((void*)(message + 2), &value, sizeof(value));
+  networkDevice.sendToDevice(networkDevice.NetworkDevice.MasterMAC, message, 1+1+4);
+
+  networkDevice.radio.powerDown();
+  Serial.println(F("[NRF24] Send data... done"));
+}
+
+int getRandomSeed() {
+  return analogRead(A0) + analogRead(A1) + analogRead(A2) + analogRead(A3) + analogRead(A4)
+    + analogRead(A0) + analogRead(A1) + analogRead(A2) + analogRead(A3) + analogRead(A4);
+}
--- src/Sensor-Passive/Sensor-Passive.ino
+++ src/Sensor-Passive/Sensor-Passive.ino
@@ -4,11 +4,18 @@
 
 SBNetwork networkDevice(true, 9, 10);
 
+/*
+ * The passive NRF24 device is receiving the request to check for a sensor value.
+ * There is no Radio.PowerDown. The power consumption with PowerSleep and ClockPreScaler is 0.35mA in sleep mode.
+ *
+ * Uncomment and configure the sensor values in the loop section
+ */
+
 void setup() {
   Serial.begin(115200);
   while(!Serial);
 
-  randomSeed(analogRead(A0) + analogRead(A1) + analogRead(A2) + analogRead(A3) + analogRead(A4));
+  randomSeed(getRandomSeed());
   SBMacAddress deviceMac(random(0,256), random(0,256), random(0,256), random(0,256), random(0,256));
   networkDevice.initialize(deviceMac);
 
@@ -18,52 +25,76 @@ void setup() {
 void loop() {
   PowerHelper::setClockPrescaler(CLOCK_PRESCALER_1);
   PowerHelper::resetADC();
-
+  
   if (Serial.available()) { char c = toupper(Serial.read()); if (c == 'N') { networkDevice.resetData(); } }
   networkDevice.update();
   
-  uint16_t voltage = Sensor::readVoltage(7, A6);
-  sendNrf24(SENSOR_TYPE_VOLTAGE, (float)voltage);
-  PowerHelper::sleep(8); // break to finish nrf24
-  
-  uint16_t humidity = Sensor::readHumidity(8, A0);
-  sendNrf24(SENSOR_TYPE_HUMIDITY, (float)humidity);
-  PowerHelper::sleep(8); // break to finish nrf24
-
-  /*
-  uint16_t photo = Sensor::readPhoto(8, A1);
-  sendNrf24(SENSOR_TYPE_PHOTO, (float)photo);
-  PowerHelper::sleep(8); // break to finish nrf24
-  
-  byte temp, hum = 0;
-  Sensor::readDht11(7, 6, temp, hum);
-  sendNrf24(SENSOR_TYPE_TEMPERATURE, (float)temp);
-  sendNrf24(SENSOR_TYPE_HUMIDITYAIR, (float)hum);
-  PowerHelper::sleep(8); // break to finish nrf24
-  */
+  uint8_t messageSize = networkDevice.available();
+  if (messageSize > 0) {
+    byte* message = (byte*)networkDevice.getMessage();
+	SBMacAddress sender = networkDevice.getLastReceivedMac();
+    Serial.print(F("Sensor value request... "));
+    
+    uint8_t version;
+    memcpy(&version, (void*)(message), sizeof(uint8_t));
+    if (version == 0x01) {
+      uint8_t type;
+      
+      memcpy(&type, (void*)(message + 1), sizeof(uint8_t));
+      Serial.println(type);
+	  switch(type) {
+        case SENSOR_TYPE_VOLTAGE:
+          uint16_t voltage = Sensor::readVoltage(7, A6);
+          sendNrf24(&sender, SENSOR_TYPE_VOLTAGE, (float)voltage);
+		  break;
+        case SENSOR_TYPE_HUMIDITY:
+          uint16_t humidity = Sensor::readHumidity(8, A0);
+          sendNrf24(&sender, SENSOR_TYPE_HUMIDITY, (float)humidity);
+		  break;
+        case SENSOR_TYPE_PHOTO:
+          uint16_t photo = Sensor::readPhoto(8, A1);
+          sendNrf24(&sender, SENSOR_TYPE_PHOTO, (float)photo);
+		  break;
+        case SENSOR_TYPE_TEMPERATURE:
+          byte temp, hum = 0;
+          Sensor::readDht11(7, 6, temp, hum);
+          sendNrf24(&sender, SENSOR_TYPE_TEMPERATURE, (float)temp);
+		  break;
+        case SENSOR_TYPE_HUMIDITYAIR:
+          byte temp, hum = 0;
+          Sensor::readDht11(7, 6, temp, hum);
+          sendNrf24(&sender, SENSOR_TYPE_HUMIDITYAIR, (float)hum);
+		  break;
+        case SENSOR_TYPE_GAS:
+		  // not implemented
+		  break;
+      }
+    }
+  }
   
   PowerHelper::setClockPrescaler(CLOCK_PRESCALER_16);
   PowerHelper::disableADC();
-  // PowerHelper::sleep(32);
-  PowerHelper::sleep(1175);
+  PowerHelper::sleep(8);
 }
 
-void sendNrf24(uint8_t type, float value) {
+void sendNrf24(SBMacAddress &sendToMac, uint8_t type, float value) {
   Serial.println(F("[NRF24] Send data... "));
   Serial.print(F("[NRF24] Type: "));
   Serial.println(type);
   Serial.print(F("[NRF24] Value: "));
   Serial.println(value);
-  networkDevice.radio.powerUp();
   
   uint8_t version = 0x01;
   byte message[1+1+4];
   memcpy((void*)(message), &version, sizeof(version));
   memcpy((void*)(message + 1), &type, sizeof(type));
   memcpy((void*)(message + 2), &value, sizeof(value));
-  networkDevice.sendToDevice(networkDevice.NetworkDevice.MasterMAC, message, 1+1+4);
+  networkDevice.sendToDevice(sendToMac, message, 1+1+4);
 
-  networkDevice.radio.powerDown();
   Serial.println(F("[NRF24] Send data... done"));
-  yield();
+}
+
+int getRandomSeed() {
+  return analogRead(A0) + analogRead(A1) + analogRead(A2) + analogRead(A3) + analogRead(A4)
+    + analogRead(A0) + analogRead(A1) + analogRead(A2) + analogRead(A3) + analogRead(A4);
 }
