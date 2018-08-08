ba28026563472d5f21e5ad8f19b150229d67c00e
Walter Heger <walter.heger@cosmoconsult.com>
2018-08-08 11:48:02 +0200

    Formatting and compiled

--- src/Sensor-Passive/Sensor-Passive.ino
+++ src/Sensor-Passive/Sensor-Passive.ino
@@ -5,18 +5,18 @@
 SBNetwork networkDevice(true, 9, 10);
 
 /*
- * The passive NRF24 device is receiving the request to check for a sensor value.
- * There is no Radio.PowerDown. The power consumption with PowerSleep and ClockPreScaler is 0.35mA in sleep mode.
- *
- * Uncomment and configure the sensor values in the loop section
- */
+   The passive NRF24 device is receiving the request to check for a sensor value.
+   There is no Radio.PowerDown. The power consumption with PowerSleep and ClockPreScaler is 0.35mA in sleep mode.
+
+   Uncomment and configure the sensor values in the loop section
+*/
 
 void setup() {
   Serial.begin(115200);
-  while(!Serial);
+  while (!Serial);
 
   randomSeed(getRandomSeed());
-  SBMacAddress deviceMac(random(0,256), random(0,256), random(0,256), random(0,256), random(0,256));
+  SBMacAddress deviceMac(random(0, 256), random(0, 256), random(0, 256), random(0, 256), random(0, 256));
   networkDevice.initialize(deviceMac);
 
   Serial.println(F("*** PRESS 'N' to reset the device"));
@@ -25,53 +25,54 @@ void setup() {
 void loop() {
   PowerHelper::setClockPrescaler(CLOCK_PRESCALER_1);
   PowerHelper::resetADC();
-  
-  if (Serial.available()) { char c = toupper(Serial.read()); if (c == 'N') { networkDevice.resetData(); } }
+
+  if (Serial.available()) {
+    char c = toupper(Serial.read());
+    if (c == 'N') {
+      networkDevice.resetData();
+    }
+  }
   networkDevice.update();
-  
+
   uint8_t messageSize = networkDevice.available();
   if (messageSize > 0) {
     byte* message = (byte*)networkDevice.getMessage();
-	SBMacAddress sender = networkDevice.getLastReceivedMac();
+    SBMacAddress sender = networkDevice.getLastReceivedMac();
     Serial.print(F("Sensor value request... "));
-    
+
     uint8_t version;
     memcpy(&version, (void*)(message), sizeof(uint8_t));
     if (version == 0x01) {
-      uint8_t type;
-      
+      uint8_t type, temp1, temp2;
+      float value;
       memcpy(&type, (void*)(message + 1), sizeof(uint8_t));
       Serial.println(type);
-	  switch(type) {
+      switch (type) {
         case SENSOR_TYPE_VOLTAGE:
-          uint16_t voltage = Sensor::readVoltage(7, A6);
-          sendNrf24(&sender, SENSOR_TYPE_VOLTAGE, (float)voltage);
-		  break;
+          value = (float)Sensor::readVoltage(7, A6);
+          break;
         case SENSOR_TYPE_HUMIDITY:
-          uint16_t humidity = Sensor::readHumidity(8, A0);
-          sendNrf24(&sender, SENSOR_TYPE_HUMIDITY, (float)humidity);
-		  break;
+          value = Sensor::readHumidity(8, A0);
+          break;
         case SENSOR_TYPE_PHOTO:
-          uint16_t photo = Sensor::readPhoto(8, A1);
-          sendNrf24(&sender, SENSOR_TYPE_PHOTO, (float)photo);
-		  break;
+          value = Sensor::readPhoto(8, A1);
+          break;
         case SENSOR_TYPE_TEMPERATURE:
-          byte temp, hum = 0;
-          Sensor::readDht11(7, 6, temp, hum);
-          sendNrf24(&sender, SENSOR_TYPE_TEMPERATURE, (float)temp);
-		  break;
+          Sensor::readDht11(7, 6, temp1, temp2);
+          value = (float)temp1;
+          break;
         case SENSOR_TYPE_HUMIDITYAIR:
-          byte temp, hum = 0;
-          Sensor::readDht11(7, 6, temp, hum);
-          sendNrf24(&sender, SENSOR_TYPE_HUMIDITYAIR, (float)hum);
-		  break;
+          Sensor::readDht11(7, 6, temp1, temp2);
+          value = (float)temp2;
+          break;
         case SENSOR_TYPE_GAS:
-		  // not implemented
-		  break;
+          // not implemented
+          break;
       }
+      if (value != NULL) sendNrf24(sender, type, value);
     }
   }
-  
+
   PowerHelper::setClockPrescaler(CLOCK_PRESCALER_16);
   PowerHelper::disableADC();
   PowerHelper::sleep(8);
@@ -83,18 +84,18 @@ void sendNrf24(SBMacAddress &sendToMac, uint8_t type, float value) {
   Serial.println(type);
   Serial.print(F("[NRF24] Value: "));
   Serial.println(value);
-  
+
   uint8_t version = 0x01;
-  byte message[1+1+4];
+  byte message[1 + 1 + 4];
   memcpy((void*)(message), &version, sizeof(version));
   memcpy((void*)(message + 1), &type, sizeof(type));
   memcpy((void*)(message + 2), &value, sizeof(value));
-  networkDevice.sendToDevice(sendToMac, message, 1+1+4);
+  networkDevice.sendToDevice(sendToMac, message, 1 + 1 + 4);
 
   Serial.println(F("[NRF24] Send data... done"));
 }
 
 int getRandomSeed() {
   return analogRead(A0) + analogRead(A1) + analogRead(A2) + analogRead(A3) + analogRead(A4)
-    + analogRead(A0) + analogRead(A1) + analogRead(A2) + analogRead(A3) + analogRead(A4);
+         + analogRead(A0) + analogRead(A1) + analogRead(A2) + analogRead(A3) + analogRead(A4);
 }
